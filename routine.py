87e74d932ac575ddd585f4ec015d68cdd0a35ba8
Gabriel Nunes <nunesgfs@gmail.com>
2023-09-05 15:10:59 -0300

    feat: limit functions defined and tested - still missing the position controlled algorithm

--- routine.py
+++ routine.py
@@ -38,24 +38,51 @@ def stop():
     inst.write(":dem:rate 1,0")
     time.sleep(0.2)
 
+# absolute factory limits of the rotary table
+def limits():
+    print("Absolute limits:")
+    print(inst.query(":limit:absolute? 1")) # rate, acc, jerk, pos_lo, pos_hi, vtrip
+
+    print("Current limits:")
+    # each mode has its limit
+    print("acceleration pos mode:", inst.query(":limit:acc? pos,1")) 
+    print("acceleration rate mode:", inst.query(":limit:acc? rate,1")) 
+    
+    print("rate pos mode:", inst.query(":limit:rate? pos,1")) 
+    print("rate rate mode:", inst.query(":limit:rate? rate,1"))     
 
 
+def set_limits(lim_acc, lim_rate=1000):
+    inst.write(f":lim:acc pos,1,{lim_acc}")
+    inst.write(f":lim:acc rate,1,{lim_acc}")
+    inst.write(f":lim:rate pos,1,{lim_rate}")
+    inst.write(f":lim:rate rate,1,{lim_rate}")
+    
+set_limits(4200)
+limits()
+
+
+# Routine implementation
 initial_status = status()
 initialize()
 
-pos_initial = initial_status["pos"]
 i_last = 50
+rate = 1000
+dt = 1 # longer is this delay, smaller is the position error
+pos_initial = initial_status["pos"]
 time_initial = time.time()
 for i in range(i_last):
     
-    inst.write(":dem:rate 1,200")
-    time.sleep(0.5)
+    inst.write(f":dem:rate 1,{rate}")
+    time.sleep(dt)
+    print(status())
     stop()
     actual_status = status()
     time.time()-time_initial
     
-    inst.write(":dem:rate 1,-200")
-    time.sleep(0.5)
+    inst.write(f":dem:rate 1,-{rate}")
+    time.sleep(dt)
+    print(status())
     stop()
     actual_status = status()
     print(f"running...{i+1}/{i_last}")
