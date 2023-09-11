97a75d25d45caf2293f7eb920405867ef6aa86ee
Gabriel Nunes <nunesgfs@gmail.com>
2023-09-11 12:51:40 -0300

    test gpib transmistion rate

---
+++ test.py
@@ -0,0 +1,24 @@
+from acutrol3000 import *
+import time
+
+n = 100
+loop_time_sum = 0
+time_initial = time.time()
+for i in range(0,n):
+    init_loop_time = time.time()
+
+    # logic to test time
+    # status()
+    read_rate()
+
+
+    loop_time = time.time() - init_loop_time    
+    loop_time_sum = loop_time_sum + loop_time
+    
+mean_loop_time = loop_time_sum/n
+final_time = time.time() - time_initial
+
+
+print("Number of requests:",n)
+print("Mean loop time: ",mean_loop_time)
+print("total time: ",final_time)
